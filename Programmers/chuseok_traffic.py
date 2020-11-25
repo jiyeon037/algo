ca03c9ca8ad22a2198c6e437a7d2ea235c2dc303
jiyeon7269@gmail.com <jiyeon7269@gmail.com>
2020-11-25 20:02:47 +0900

    추석 트래픽

---
+++ Programmers/chuseok_traffic.py
@@ -0,0 +1,23 @@
+def solution(lines):
+    times = []
+    for line in lines:
+        date, time, during = line.split()
+        time = time.split(':')
+        
+        end_time = int(time[0]) * 3600 + int(time[1]) * 60 + float(time[2])
+        start_time = end_time - float(during[:-1]) + 0.001
+        start_time = round(start_time, 3)
+        times.append([start_time, end_time])
+
+    max_ans = 0
+    for time in times:
+        for t in time:
+            for dt in -1, 1:
+                start, end = list(sorted([t, t+dt]))
+                ans = 0
+                for s, e in times:
+                    if start <= e and s < end:
+                        ans += 1
+                max_ans = max(max_ans, ans)
+
+    return max_ans
