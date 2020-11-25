0dd6a0acfcbbbe0403be9b3dfc094d52e4b1aff5
jiyeon7269@gmail.com <jiyeon7269@gmail.com>
2020-11-25 14:04:22 +0900

    추석 트래픽

--- Programmers/chuseok_traffic.py
+++ Programmers/chuseok_traffic.py
@@ -1,14 +1,15 @@
 def solution(lines):
-    max_ans = 0
     times = []
     for line in lines:
         date, time, during = line.split()
         time = time.split(':')
+        
         end_time = int(time[0]) * 3600 + int(time[1]) * 60 + float(time[2])
         start_time = end_time - float(during[:-1]) + 0.001
         start_time = round(start_time, 3)
         times.append([start_time, end_time])
 
+    max_ans = 0
     for time in times:
         for t in time:
             for dt in -1, 1:
