00821d3ed7b619d9a1d8024ba720fb4d2d17dfbb
jiyeon7269@gmail.com <jiyeon7269@gmail.com>
2021-07-03 22:27:23 +0900

    [1차] 프렌즈4블록

--- Study/21-06-26/friends_4block.py
+++ Study/21-06-26/friends_4block.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def solution(m, n, board):
     answer = 0
 
@@ -7,20 +9,21 @@ def solution(m, n, board):
     
     while True:
         counted = []
-        flag = False
         for i in range(m-1):
             for j in range(n-1):
+                if board[i][j] == '0':
+                    continue
                 if board[i][j] == board[i][j+1]:
-                    if board[i][j] == board[i+1][j] == board[i+1][j+1] != '0':
-                        flag = True
+                    if board[i][j] == board[i+1][j] == board[i+1][j+1]:
                         counted.append((i,j))
                         counted.append((i,j+1))
                         counted.append((i+1,j))
                         counted.append((i+1,j+1))
 
-        if flag == False:
+        if not counted:
             break
-
+        
+        print(len(set(counted)))
         answer += len(set(counted))
         for c in counted:
             board[c[0]][c[1]] = '0'
@@ -28,20 +31,21 @@ def solution(m, n, board):
         for i in range(m):
             for j in range(n):
                 if board[i][j] == '0':
+                    queue = deque()
+                    for col in range(n):
+                        for row in range(m-1, -1, -1):
+                            if board[row][col] != '0':
+                                queue.append(board[row][col])
+                                board[row][col] = '0'
 
-                    for row in range(m-1, -1, -1):
-                        for col in range(n):
-                            if board[row][col] == '0':
-                                for k in range(row-1,-1,-1):
-                                    if board[k][col] != '0':
-                                        tmp = board[k][col]
-                                        board[k][col] = board[row][col]
-                                        board[row][col] = tmp
-                                        break
+                        for row in range(m-1, -1, -1):
+                            if queue:
+                                board[row][col] = queue.popleft()
+
     return answer
 
 
-
-board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+board = ["AAA","AAA","AAA"]
+#board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
 #board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
-print(solution(4,5,board))
+print(solution(3,3,board))
